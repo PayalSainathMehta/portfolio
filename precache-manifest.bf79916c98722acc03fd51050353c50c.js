7ad08d3ef740fbf65225d5d45c5f99375642793e
Utkarsh Garg <utkarsh_garg@live.com>
2020-03-14 18:15:35 -0400

    Updates

---
+++ precache-manifest.bf79916c98722acc03fd51050353c50c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b004d2889538074d8e402c24c502308d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "02821bc45b7fe465a670",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f3cbb766ea959300d23e",
+    "url": "/portfolio/static/css/main.1f7ed24e.chunk.css"
+  },
+  {
+    "revision": "02821bc45b7fe465a670",
+    "url": "/portfolio/static/js/2.54304fa0.chunk.js"
+  },
+  {
+    "revision": "cf5689887d395d116a38769473339df4",
+    "url": "/portfolio/static/js/2.54304fa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3cbb766ea959300d23e",
+    "url": "/portfolio/static/js/main.81a340e3.chunk.js"
+  },
+  {
+    "revision": "6001bb842c853c9f76dc",
+    "url": "/portfolio/static/js/runtime-main.c9f1ecd2.js"
+  },
+  {
+    "revision": "824547d28a72a504622166ac4996f5ac",
+    "url": "/portfolio/static/media/resume.824547d2.pdf"
+  }
+]);
