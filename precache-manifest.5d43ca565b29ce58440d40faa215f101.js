3bf1bd6427d2e9b6a99888b9cac102693750eea2
PayalSainathMehta <psmehta@cs.stonybrook.edu>
2020-03-14 21:24:43 -0400

    Updates

---
+++ precache-manifest.5d43ca565b29ce58440d40faa215f101.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05183b53c388790a07b0eb63c557cb9d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f0ebcc9085fb4d8d49d9",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a26660fead27a1cda66d",
+    "url": "/portfolio/static/css/main.25338be6.chunk.css"
+  },
+  {
+    "revision": "f0ebcc9085fb4d8d49d9",
+    "url": "/portfolio/static/js/2.53ed34d2.chunk.js"
+  },
+  {
+    "revision": "cf5689887d395d116a38769473339df4",
+    "url": "/portfolio/static/js/2.53ed34d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26660fead27a1cda66d",
+    "url": "/portfolio/static/js/main.2c6a6240.chunk.js"
+  },
+  {
+    "revision": "c4322a7adae57c8cb5cd",
+    "url": "/portfolio/static/js/runtime-main.dbf595e6.js"
+  },
+  {
+    "revision": "61fd5ee9e0329c012a4a0dae2f1b30ec",
+    "url": "/portfolio/static/media/resume.61fd5ee9.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.22659137399c3824afdf72b726af542b.js"
+  "/portfolio/precache-manifest.5d43ca565b29ce58440d40faa215f101.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
